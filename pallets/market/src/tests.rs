89cde1711208ecd500e764bf4074c629e63a05a3
BoredApe8461 <letesinkelegesse@gmail.com>
2024-05-23 17:22:43 +0000

    test update_region_price

--- pallets/market/src/tests.rs
+++ pallets/market/src/tests.rs
@@ -162,3 +162,53 @@ fn unlist_region_works() {
 		System::assert_last_event(Event::Unlisted { region_id }.into())
 	});
 }
+
+#[test]
+fn update_price_works() {
+	new_test_ext().execute_with(|| {
+		let region_id = RegionId { begin: 0, core: 0, mask: CoreMask::complete() };
+		let seller = 2;
+		let signer = RuntimeOrigin::signed(seller);
+
+		assert_ok!(Regions::mint_into(&region_id.into(), &seller));
+
+		let record: RegionRecord<u64, u64> = RegionRecord { end: 8, owner: 1, paid: None };
+		let timeslice: u64 = <Test as crate::Config>::TimeslicePeriod::get();
+		let price = 1_000_000;
+		let recipient = 1;
+		let new_price = 2_000_000;
+
+		assert_ok!(Regions::set_record(region_id, record.clone()));
+
+		// Failure: NotListed
+		assert_err!(
+			Market::update_region_price(signer.clone(), region_id, new_price),
+			Error::<Test>::NotListed
+		);
+
+		assert_ok!(Market::list_region(signer.clone(), region_id, price, Some(recipient)));
+
+		// Failure: NotAllowed - only the seller can update the price
+		assert_err!(
+			Market::update_region_price(RuntimeOrigin::signed(3), region_id, new_price),
+			Error::<Test>::NotAllowed
+		);
+
+		// Failure: RegionExpired
+		RelayBlockNumber::set(10 * timeslice);
+		assert_err!(
+			Market::update_region_price(signer.clone(), region_id, new_price),
+			Error::<Test>::RegionExpired
+		);
+
+		// Should be working now
+		RelayBlockNumber::set(2 * timeslice);
+		assert_ok!(Market::update_region_price(signer, region_id, new_price));
+
+		// Check storage
+		assert_eq!(
+			Market::listings(region_id),
+			Some(Listing { seller, timeslice_price: new_price, sale_recipient: recipient })
+		);
+	});
+}
