8c418820f9e21aad11a90d7a7fe6c117572c188e
BoredApe8461 <letesinkelegesse@gmail.com>
2024-08-11 12:10:53 +0200

    Cocos release v2.0.1

--- runtime/cocos/src/lib.rs
+++ runtime/cocos/src/lib.rs
@@ -194,7 +194,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("regionx-parachain"),
 	impl_name: create_runtime_str!("regionx-parachain"),
 	authoring_version: 1,
-	spec_version: 2_000_000,
+	spec_version: 2_000_001,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
